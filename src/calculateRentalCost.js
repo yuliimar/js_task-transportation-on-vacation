809b56ce749b4822d42f804509fe3ff8ab4e4a14
YULIIA M <yuliiamartynovych@gmail.com>
2025-04-29 16:23:23 +0200

    Add Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,42 +5,23 @@
  */
 function calculateRentalCost(days) {
   const DAILY_RATE = 80;
-  const THREE_DAY_THRESHOLD = 3;
-  const SIX_DAY_THRESHOLD = 6;
-  const WEEKLY_THRESHOLD = 7;
+  const THREE_DAY_DISCOUNT = 100;
+  const SIX_DAY_DISCOUNT = 220;
+  const WEEKLY_DISCOUNT = 230;
 
-  const THREE_DAY_DISCOUNT_PRICE = 100;
-  const SIX_DAY_DISCOUNT_PRICE = 220;
-  const WEEKLY_DISCOUNT_PRICE = 230;
-
-  if (days === THREE_DAY_THRESHOLD) {
-    return THREE_DAY_DISCOUNT_PRICE;
+  if (days === 3) {
+    return THREE_DAY_DISCOUNT;
   }
 
-  if (days === SIX_DAY_THRESHOLD) {
-    return SIX_DAY_DISCOUNT_PRICE;
+  if (days === 6) {
+    return SIX_DAY_DISCOUNT;
   }
 
-  if (days === WEEKLY_THRESHOLD) {
-    return WEEKLY_DISCOUNT_PRICE;
+  if (days === 7) {
+    return WEEKLY_DISCOUNT;
   }
 
-  if (days > WEEKLY_THRESHOLD) {
-    const weeks = Math.floor(days / 7);
-    const remainingDays = days % 7;
-
-    return weeks * WEEKLY_DISCOUNT_PRICE + calculateRentalCost(remainingDays);
-  }
-
-  if (days > SIX_DAY_THRESHOLD) {
-    return WEEKLY_DISCOUNT_PRICE + (days - WEEKLY_THRESHOLD) * DAILY_RATE;
-  }
-
-  if (days > THREE_DAY_THRESHOLD) {
-    return THREE_DAY_DISCOUNT_PRICE + (days - THREE_DAY_THRESHOLD) * DAILY_RATE;
-  }
-
-  return 80;
+  return DAILY_RATE;
 }
 
 module.exports = calculateRentalCost;
